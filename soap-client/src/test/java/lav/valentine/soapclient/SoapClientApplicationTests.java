d678b9bcb217ce0e179d18366028f70bd8e9a423
LaValentine <86903020+LaValentine@users.noreply.github.com>
2022-05-28 15:56:04 +0400

    Soap-client module added

---
+++ soap-client/src/test/java/lav/valentine/soapclient/SoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package lav.valentine.soapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
